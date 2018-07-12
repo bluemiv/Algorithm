29812e2be60f8f741392b0ecd2880731b4bae127
TaehongKim_mac <7538518@naver.com>
2018-07-12 19:12:04 +0900

    백준 11725번 BFS, DFS

---
+++ src/baekjoon/java02/Ex15680.java
@@ -0,0 +1,13 @@
+package baekjoon.java02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex15680 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine().equals("0")?"YONSEI":"Leading the Way to the Future");
+	}
+}
