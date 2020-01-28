c728ba7a6e976245ab78c0e7016d0315bb9cb056
TaehongKim <taehongkim@TaehongKimui-MacBookPro.local>
2020-01-28 22:07:39 +0900

    필요없는 파일 제거

--- python/baekjoon/Ex1000.py
+++
@@ -1,3 +0,0 @@
-import sys
-_ = map(int, sys.stdin.readline().split())
-print(sum(_))
--- python/baekjoon/ex10951.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-
-if __name__ == "__main__":
-    _input = "init"
-    while _input:
-        _input = sys.stdin.readline().strip()
-        if _input:
-            print(sum(list(map(int, _input.split()))))
