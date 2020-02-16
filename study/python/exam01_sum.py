4adaa0c694d5c042977113c44a035753d1de2cc1
taehong.kim <public.devhong@gmail.com>
2020-02-16 20:47:02 +0900

    Update exam01_sum.py

--- study/python/exam01_sum.py
+++ study/python/exam01_sum.py
@@ -8,15 +8,15 @@ class Exam01(unittest.TestCase):
     def test_sum(self):
         """1 부터 n 까지 더하는 알고리즘"""
         answer = 0  # 정답
-        for _ in range(1, 11):
+        for _ in range(1, 101):
             answer += _  # 1 부터 10 까지 더한다.
-        self.assertEqual(answer, 55)
+        self.assertEqual(answer, 5050)
 
     def test_sum2(self):
         """가우스 방법으로 문제 풀기"""
-        n = 100
+        n = 100000000
         answer = n * (n+1) // 2
-        self.assertEqual(answer, 5050)
+        self.assertEqual(answer, 5000000050000000)
 
 
 if __name__ == "__main__":
