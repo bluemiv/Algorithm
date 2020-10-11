1a13d3668e99ad5261bb82227abaf0b1f1f66498
TaehongKim <public.devhong@gmail.com>
2020-10-11 17:25:55 +0900

    CodeUp Ex1008

---
+++ codeup/java/src/Ex1008.java
@@ -0,0 +1,18 @@
+public class Ex1008 {
+    public static void main(String[] args) {
+        final char[][] arr = {
+                {'\u250C', '\u252C', '\u2510'},
+                {'\u251C', '\u253C', '\u2524'},
+                {'\u2514', '\u2534', '\u2518'}
+        };
+
+        final StringBuilder sb = new StringBuilder();
+        for(char[] cs : arr) {
+            for (char c : cs) {
+                sb.append(c);
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
