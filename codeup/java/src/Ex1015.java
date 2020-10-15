815139c7e0944f37f44cf7b2f8954995dbc54220
taehong.kim <public.devhong@gmail.com>
2020-10-15 18:32:16 +0900

    Add Ex1015

---
+++ codeup/java/src/Ex1015.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex1015 {
+    public static void main(String[] args) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final String input = br.readLine() + " ";
+        System.out.println(input + input + input);
+    }
+}
+
