50ba261444b4706ac63adda17f76700bf192100b
taehong.kim <public.devhong@gmail.com>
2020-10-15 18:34:36 +0900

    Rename files

--- codeup/java/src/Ex1017.java
+++ codeup/java/src/Ex1017.java
@@ -5,26 +5,8 @@ import java.io.InputStreamReader;
 public class Ex1017 {
     public static void main(String[] args) throws IOException {
         final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        final String input = br.readLine();
-        final String[] dateInfo = input.split("\\.");
-        final String year = parsingYear(dateInfo[0]);
-        final String month = parsingMonth(dateInfo[1]);
-        final String date = parsingMonth(dateInfo[2]);
-        System.out.printf("%s.%s.%s", year, month, date);
-    }
-
-    public static String parsingYear(String num) {
-        if (num.length() == 2) {
-            return "00" + num;
-        }
-        return num;
-    }
-
-    public static String parsingMonth(String num) {
-        if (num.length() == 1) {
-            return "0" + num;
-        }
-        return num;
+        final String input = br.readLine() + " ";
+        System.out.println(input + input + input);
     }
 }
 
--- codeup/java/src/Ex1018.java
+++ codeup/java/src/Ex1018.java
@@ -2,11 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Ex1015 {
+public class Ex1018 {
     public static void main(String[] args) throws IOException {
         final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        final String input = br.readLine() + " ";
-        System.out.println(input + input + input);
+        System.out.println(br.readLine());
     }
 }
-
