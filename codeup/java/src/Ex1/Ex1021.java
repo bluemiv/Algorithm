01aa554d919b26a051f75b75f449c8cb4c545b95
taehong.kim <public.devhong@gmail.com>
2020-10-15 20:46:13 +0900

    Add Ex1020 ~ Ex1022

---
+++ codeup/java/src/Ex1/Ex1021.java
@@ -0,0 +1,12 @@
+package Ex1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex1021 {
+    public static void main(String[] args) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine());
+    }
+}
