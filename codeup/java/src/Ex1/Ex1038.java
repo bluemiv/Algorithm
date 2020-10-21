6ee75f43a076d4f65492599d2e54f723d938b61b
TaehongKim <public.devhong@gmail.com>
2020-10-21 21:47:15 +0900

    Add Ex1038

---
+++ codeup/java/src/Ex1/Ex1038.java
@@ -0,0 +1,13 @@
+package Ex1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex1037 {
+    public static void main(String[] args) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final int input = Integer.parseInt(br.readLine());
+        System.out.println((char) input);
+    }
+}
