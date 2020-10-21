3f497f7a135d88d60619f952d542820637dbe0aa
TaehongKim <public.devhong@gmail.com>
2020-10-21 21:28:07 +0900

    Add Ex1036

---
+++ codeup/java/src/Ex1/Ex1036.java
@@ -0,0 +1,13 @@
+package Ex1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex1036 {
+    public static void main(String[] args) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char input = br.readLine().toCharArray()[0];
+        System.out.println((int)input);
+    }
+}
