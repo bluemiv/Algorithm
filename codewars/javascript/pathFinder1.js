87123b4cb01dc40dde6465a144d31266765e1143
Taehong Kim <public.bluemiv@gmail.com>
2022-03-03 17:35:02 +0900

    Update pathFinder1.js

--- codewars/javascript/pathFinder1.js
+++ codewars/javascript/pathFinder1.js
@@ -1,5 +1,7 @@
 // https://www.codewars.com/kata/5765870e190b1472ec0022a2/javascript
 
+const dir = [[-1, 0], [0, -1], [1, 0], [0, 1]];
+
 class Node {
   constructor(x, y) {
     this.x = x;
@@ -7,59 +9,34 @@ class Node {
   }
 }
 
-const invalidValue = (x, y, n) => {
-  return x < 0 || x > n-1 || y < 0 || y > n-1;
-}
-
 const bfs = (x, y, n, graph, visited) => {
   const queue = [];
-  const directions = [[-1, 0], [0, -1], [1, 0], [0, 1]];
-  let isResult = false;
-  
-  const firstNode = new Node(x, y);
+
+  queue.push(new Node(x, y));
   visited[x][y] = true;
   
-  queue.push(firstNode);
   while(queue.length > 0) {
     const curNode = queue.pop();
-    directions.forEach(dir => {
-      const [newX, newY] = [curNode.x + dir[0], curNode.y + dir[1]];
+
+    for(let i = 0; i < dir.length; i++) {
+     const [newX, newY] = [curNode.x + dir[i][0], curNode.y + dir[i][1]];
         
-      if(newX === n-1 && newY === n-1) {
-        isResult = true;
-        return;
-      }
+      if(newX === n - 1 && newY === n - 1) return true;
 
-      if(
-        !invalidValue(newX, newY, n) // 올바른 배열 범위이어야 함
-        && graph[newX][newY] // '벽'이 아니어야 함
-        && !visited[newX][newY] // 방문안했어야 함
-      ) {  
-        visited[newX][newY] = true;
-        const nextNode = new Node(newX, newY);
-        queue.push(nextNode);        
-      }
-    })
+      if(!(newX < 0 || newX > n-1 || newY < 0 || newY > n - 1) && graph[newX][newY] && !visited[newX][newY]) {
+        queue.push(new Node(newX, newY));
+        visited[newX][newY] = true; 
+      } 
+    }
   }
-  return isResult;
-}
-
-const makeGraph = (maze) => {
-  return maze.split('\n').map(row => row.split('').map(e => e === '.'))
-}
-
-const initVisited = (n) => {
-  return new Array(n).fill(0).map(e => new Array(n).fill(false));
+  
+  return false;
 }
 
 function pathFinder(maze){
-  // 그래프 생성
-  const graph = makeGraph(maze);
+  const graph = maze.split('\n').map(row => row.split('').map(e => e === '.'));
   const n = graph.length;
+  const visited = new Array(n).fill(0).map(_ => new Array(n).fill(false));
   
-  // 노드를 방문했는지 확인하는 flag 값
-  const visited = initVisited(n);
-  
-  const result = bfs(0, 0, n, graph, visited);
-  return result;
+  return bfs(0, 0, n, graph, visited);
 }
