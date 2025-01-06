e8c6822dc794a5bdf4976d2c978f27c74398f328
Taehong Kim <public.bluemiv@gmail.com>
2025-01-06 23:19:20 +0900

    [Bronze II] Title: 분해합, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,7 +1,9 @@
-n = int(input())
+strnum = input()
+n = int(strnum)
 
+minnum = len(strnum) * 10
 ret = 0
-for i in range(1, n):
+for i in range(n - minnum if n > minnum else 1, n):
     if n == i + sum(map(int, list(str(i)))):
         ret = i
         break
