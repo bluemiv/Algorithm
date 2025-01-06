1905683638779545d36f7c9cfb58a6f88f461c2a
Taehong Kim <public.bluemiv@gmail.com>
2025-01-06 23:03:32 +0900

    [Bronze II] Title: 블랙잭, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,12 +1,12 @@
 n, m = map(int, input().split())
 nums = sorted(list(map(int, input().split())))
 
-numset = set()
+maxnum = 0
 for i in range(n - 2):
     for j in range(i + 1, n - 1):
         for k in range(j + 1, n):
             s = nums[i] + nums[j] + nums[k]
             if s <= m:
-                numset.add(s)
+                maxnum = max(maxnum, s)
 
-print(max(numset))
+print(maxnum)
