37bbb1abaf663a89f5b08fa922e7972536b26141
Taehong Kim <public.bluemiv@gmail.com>
2025-01-06 22:54:32 +0900

    Update 소수 찾기.py

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,4 +1,4 @@
-def is_primary(n):
+def is_prime(n):
     if n == 1:
         return False
     for i in range(2, n // 2 + 1):
@@ -11,6 +11,6 @@ input()
 nums = list(map(int, input().split()))
 cnt = 0
 for n in nums:
-    if is_primary(n):
+    if is_prime(n):
         cnt += 1
 print(cnt)
