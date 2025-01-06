f4cee67cb4120de23e1624f74d1ffb6403931525
Taehong Kim <public.bluemiv@gmail.com>
2025-01-06 22:40:20 +0900

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,16 @@
+def is_primary(n):
+    if n == 1:
+        return False
+    for i in range(2, n // 2 + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+input()
+nums = list(map(int, input().split()))
+cnt = 0
+for n in nums:
+    if is_primary(n):
+        cnt += 1
+print(cnt)
