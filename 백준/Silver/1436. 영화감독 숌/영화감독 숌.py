751c4cf6fce70df559e61f9b46b1b53d63d9bffe
Taehong Kim <public.bluemiv@gmail.com>
2025-01-07 09:08:06 +0900

    [Silver V] Title: 영화감독 숌, Time: 556 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -5,7 +5,13 @@ i = 666
 while True:
     if "666" in str(i):
         cnt += 1
-    if cnt == n:
-        break
-    i += 1
+        if cnt == n:
+            break
+
+        if i <= 5000:
+            i += 1000
+        else:
+            i += 1
+    else:
+        i += 1
 print(i)
