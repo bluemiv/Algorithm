4a7c7cc758d56887a570fe7fb0f08f88a9606232
Taehong Kim <public.bluemiv@gmail.com>
2025-01-08 14:24:50 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
@@ -5,16 +5,12 @@ input = sys.stdin.readline
 n = int(input())
 line = list(map(int, input().strip().split()))
 
-cur = 1
+cnt = 1
 stack = []
-while line or stack:
-    if not line and stack and stack[-1] != cur:
-        break
-    while stack and stack[-1] == cur:
+for student in line:
+    stack.append(student)
+    while stack and stack[-1] == cnt:
         stack.pop()
-        cur += 1
+        cnt += 1
 
-    if line:
-        student = line.pop(0)
-        stack.append(student)
 print("Sad" if stack else "Nice")
